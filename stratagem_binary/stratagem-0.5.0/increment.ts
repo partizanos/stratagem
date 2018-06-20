bd490c3d767333bd6388692138abcfbfeac64aa6
partizanos <dimi.proios@gmail.com>
2018-06-21 01:03:19 +0200

    add succesor with with variable example

--- stratagem_binary/stratagem-0.5.0/increment.ts
+++ stratagem_binary/stratagem-0.5.0/increment.ts
@@ -16,6 +16,8 @@ suc: nat -> nat
 zero: nat
 
 Variables 
+x : nat
+y : nat
 n : name
 n1 : name
 p : pp
@@ -27,8 +29,8 @@ Try(S) = Choice(S, Identity)
 DoForAllPlaces(V) = Union(Try(V), Choice(One(DoForAllPlaces(V), 3), Try(V)))
 DoForLastPlace(V) = Choice(One(DoForLastPlace(V), 3), V)
 
-t1 = { Place(place1, suc(zero), Place(place2, zero, $p)) -> Place(place1, zero, Place(place2, suc(zero), $p)) }
-t2 = { Place(place2, suc(zero), Place(place1, zero, $p)) -> Place(place2, zero, Place(place1, suc(zero), $p)) }
+t1 = { Place(place1, suc($x), Place(place2, $y, $p)) -> Place(place1, $x, Place(place2, suc($y), $p)) }
+t2 = { Place(place2, suc($x), Place(place1, $y, $p)) -> Place(place2, $x, Place(place1, suc($y), $p)) }
 
 Transitions
 makeTransition = DoForAllPlaces(t1())

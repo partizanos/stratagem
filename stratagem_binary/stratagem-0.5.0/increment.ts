742d3b56bb40057a5c36c151cabcc1c1f1544c43
partizanos <dimi.proios@gmail.com>
2018-06-27 02:51:55 +0200

    apply same pre/post condition to verify if a state exsts

--- stratagem_binary/stratagem-0.5.0/increment.ts
+++ stratagem_binary/stratagem-0.5.0/increment.ts
@@ -16,11 +16,16 @@ noPlace : pp
 Place: name, nat, pp -> pp
 suc: nat -> nat
 zero: nat
+
 Variables
 jeton_p1: nat
 jeton_p2: nat
 jeton_p3: nat
+p1a: nat
+p2a: nat
+p3a: nat
 
+p0 : pp
 p : pp
 initialState = Place(p1, suc(zero), Place(p2, zero, Place(p3, zero, noPlace)))
 
@@ -30,18 +35,27 @@ Try(S) = Choice(S, Identity)
 DoForAllPlaces(V) = Union(Try(V), Choice(One(DoForAllPlaces(V),33), Try(V)))
 
 t1 = { 
-    Place(p1, suc($jeton_p1), Place(p2, $jeton_p2, $p)) 
+    Place(p1, suc($jeton_p1), Place(p2, $jeton_p2, Place(p3, $jeton_p3, $p))) 
     -> 
-    Place(p1, $jeton_p1, Place(p2, suc($jeton_p2), $p)) 
+    Place(p1, suc($jeton_p1), Place(p2, $jeton_p2, Place(p3, $jeton_p3, $p))) 
 }
 
-t2 = { Place(p2, suc($jeton_p2), Place(p1, $jeton_p1, $p)) -> Place(p2, $jeton_p2, Place(p1, suc($jeton_p1), $p)) }
-// forced to write all state in all transition because p1 p2 p3 initial state definition, doens combine p1, p3
-t3 = { Place(p1, suc($jeton_p1), Place(p2, $jeton_p2, Place(p3, $jeton_p3, $p))) -> Place(p1, $jeton_p1, Place(p2, $jeton_p2, Place(p3, suc($jeton_p3), $p))) }
+t2 = { Place(p1, $jeton_p1, Place(p2, suc($jeton_p2), Place(p3, $jeton_p3, $p))) 
+    -> Place(p1, $jeton_p1, Place(p2, $jeton_p2, Place(p3, $jeton_p3, $p)))  }
+    // forced to write all state in all transition because p1 p2 p3 initial state definition, doens combine p1, p3
+t3 = { Place(p1, suc($jeton_p1), Place(p2, $jeton_p2, Place(p3, $jeton_p3, $p))) 
+    -> Place(p1, $jeton_p1, Place(p2, $jeton_p2, Place(p3, suc($jeton_p3), $p))) }
 
 // infinite Loop
 // checkProp = { Place(p1, suc($jeton_p1), $p) -> Place(p1, suc(suc($jeton_p1)), $p) }
-checkProp = { Place(p1, suc($jeton_p1), $p) -> Place(p1, $jeton_p1, $p) }
+checkProp = {  
+    Place(p1,  $jeton_p1, 
+        Place(p2, zero, 
+            Place(p3, $jeton_p3, noPlace))) 
+    -> 
+    Place(p1,  $jeton_p1, 
+        Place(p2, suc(suc(zero)), 
+            Place(p3, $jeton_p3, noPlace))) }
 
 allStrat = Union(Union(Try(t1()), Try(t2())), t3())
 StateSpaceStrat = Fixpoint(Union(Identity, allStrat()))
